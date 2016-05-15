0103724d74fbcf239bbd294f31cbb65ffba9b5a5
Joshua Richardson <richardsonjoshua228@gmail.com>
2016-05-15 12:59:54 +0100

    no message

--- static/js/learn.js
+++ static/js/learn.js
@@ -7,33 +7,31 @@ function populateConcepts(){
 
 		$(".collapsible").append('<li id="ch'+(j+1)+'"><div class="collapsible-header"><b>'+data[j]["concept"]+'</b></div></li>');
 		$("#ch"+(j+1)).click(
+
 			function(o){
-				var querydata = $(this).text()
+			  var divid = makeid();
+			  var querydata = $(this).text()
 		   	  var posting = $.post( "/api/v1/wikisummary", {"query": querydata, "sentences": "2" } );
 		   	  posting.done(function( data ) {
 		   	  		 var response = (jQuery.parseJSON(data))['content'];
+					 var ele = $("#"+o.currentTarget.id);
+		   			 ele.children().next().remove();
+		   		     ele.append('<div id=\"' + divid + '\" class="collapsible-body"><p>' + JSON.stringify(response) + '</p><img class="video-placeholder" src="/static/img/video-placeholder.png"></div>');
+					 ele.children().next().show();
 
-		   			 $("#"+o.currentTarget.id).children().next().remove();
-		   		     $("#"+o.currentTarget.id).append('<div class="collapsible-body"><p>' + JSON.stringify(response) + '</p><img class="video-placeholder" src="/static/img/video-placeholder.png">');
-					 $("#"+o.currentTarget.id).children().next().show();
+					   $.get( "/api/v1/concept/" + encodeURIComponent(querydata), function( data ) {
+							var response = (jQuery.parseJSON(data));
+							var similars = JSON.stringify(response['subs']).split(",");
 
-			   $.get( "/api/v1/concept/" + encodeURIComponent(querydata), function( data ) {
-				 	var response = (jQuery.parseJSON(data));
-				 	var similars = response['subs'];
-				 	alert(similars);
+							for (var x = 0; x < similars.length; x++) {
+								$("#" + divid).append('<a href=\"#\">' + similars[x] + '</a>');
+							}
 
-				 	for (var x = 0; x < similars.length; x++) {
-
-				 		$("#"+o.currentTarget.id).append('<a href=\"#\">' + JSON.stringify(similars[x]) + '</p>');
-				 	}
-					$("#"+o.currentTarget.id).append('</div>');
-					alert(JSON.stringify(response));
-
-//				    $( ".result" ).html( data );
-//					 $("#"+o.currentTarget.id).append('<div class="collapsible-body"><p>' + JSON.stringify(response) + '</p><img class="video-placeholder" src="/static/img/video-placeholder.png"></div>');
+		//				    $( ".result" ).html( data );
+		//					 $("#"+o.currentTarget.id).append('<div class="collapsible-body"><p>' + JSON.stringify(response) + '</p><img class="video-placeholder" src="/static/img/video-placeholder.png"></div>');
 
 
-			  });
+					  });
 		   	  });
 
 
@@ -74,3 +72,14 @@ $(document).ready(function(){
 	//populateSummary();
 
 });
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
