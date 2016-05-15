3ea5246e4875151650c86ebe9a3cde3bc9c28387
Joshua Richardson <richardsonjoshua228@gmail.com>
2016-05-15 12:38:49 +0100

    no message

--- static/js/learn.js
+++ static/js/learn.js
@@ -8,26 +8,35 @@ function populateConcepts(){
 		$(".collapsible").append('<li id="ch'+(j+1)+'"><div class="collapsible-header"><b>'+data[j]["concept"]+'</b></div></li>');
 		$("#ch"+(j+1)).click(
 			function(o){
-
-
-		   	  var posting = $.post( "/api/v1/wikisummary", {"query": $(this).text(), "sentences": "2" } );
+				var querydata = $(this).text()
+		   	  var posting = $.post( "/api/v1/wikisummary", {"query": querydata, "sentences": "2" } );
 		   	  posting.done(function( data ) {
 		   	  		 var response = (jQuery.parseJSON(data))['content'];
 
 		   			 $("#"+o.currentTarget.id).children().next().remove();
-		   		     $("#"+o.currentTarget.id).append('<div class="collapsible-body"><p>' + JSON.stringify(response) + '</p><img class="video-placeholder" src="/static/img/video-placeholder.png"></div>');
+		   		     $("#"+o.currentTarget.id).append('<div class="collapsible-body"><p>' + JSON.stringify(response) + '</p><img class="video-placeholder" src="/static/img/video-placeholder.png">');
 					 $("#"+o.currentTarget.id).children().next().show();
-		   	  });
 
-		   	  $.get( "/api/v1/concept/" + $(this).text(), function( data ) {
+			   $.get( "/api/v1/concept/" + encodeURIComponent(querydata), function( data ) {
 				 	var response = (jQuery.parseJSON(data));
-					alert(JSON.stringify(response))
+				 	var similars = response['subs'];
+				 	alert(similars);
+
+				 	for (var x = 0; x < similars.length; x++) {
+
+				 		$("#"+o.currentTarget.id).append('<a href=\"#\">' + JSON.stringify(similars[x]) + '</p>');
+				 	}
+					$("#"+o.currentTarget.id).append('</div>');
+					alert(JSON.stringify(response));
 
 //				    $( ".result" ).html( data );
 //					 $("#"+o.currentTarget.id).append('<div class="collapsible-body"><p>' + JSON.stringify(response) + '</p><img class="video-placeholder" src="/static/img/video-placeholder.png"></div>');
 
 
 			  });
+		   	  });
+
+
 
 			}
 		);
